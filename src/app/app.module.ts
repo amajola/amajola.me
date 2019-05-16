4c5690dccd4a7e9e79f111f5e1c5eb61841ac00f
Joshua Britz <joshua@onepointfour.io>
2019-05-16 10:45:13 +0200

    Tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CdkScrollingModule } from './helpers/scrolling/scrolling.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -11,9 +10,11 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MyWorkComponent } from './pages/my-work/my-work.component';
 import { SkillsComponent } from './pages/skills/skills.component';
-import { DrawingboardComponent } from './components/drawingboard/drawingboard.component';
-import { NavigationDotsComponent } from './components/navigation-dots/navigation-dots.component';
-import { ButtonComponent } from './components/button/button.component';
+
+import { JumbotronComponent } from '@components/jumbotron';
+import { DrawingboardComponent } from '@components/drawingboard';
+import { NavigationDotsComponent } from '@components/navigation-dots';
+import { ButtonComponent } from '@components/button';
 
 @NgModule({
   declarations: [
---
+++ src/app/components/drawingboard/index.ts
@@ -0,0 +1,1 @@
+export { DrawingboardComponent } from './drawingboard.component';
