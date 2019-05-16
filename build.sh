539be2a4ed36db2b83c9e853ba4e1a1d5473aabb
Joshua Britz <joshua@onepointfour.io>
2019-05-16 08:39:02 +0200

    Build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #! /bin/bash
-yarn build
+
+echo 💡 BUILDING APP
+echo 💡 Log files in ./build-logs
+
+mkdir -p build-logs
+
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+yarn build --prod > ./build-logs/build.$today.log
+
+echo ''
+echo ☄️ DONE ☄️
